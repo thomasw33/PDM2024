8dac73b31b0c15e6249eb08d1006374eb8502d15
thomasw33 <144080706+thomasw33@users.noreply.github.com>
2024-03-19 14:18:04 -0500

    Update sketch.js

--- Assignment2.4/sketch.js
+++ Assignment2.4/sketch.js
@@ -31,7 +31,7 @@ function setup() {
   // Create the slider
   filterSlider = createSlider(100, 5000, 3000, 1);
   filterSlider.position(150, 200);
-  filterSlider.changed(() => {
+  filterSlider.mouseMoved(() => {
     filter.frequency.value = filterSlider.value();
   });
 }
