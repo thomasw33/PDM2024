08f8e1bda8cf5f91eb8eba05ceb7cf5ff96c16a6
thomasw33 <144080706+thomasw33@users.noreply.github.com>
2024-03-19 14:19:29 -0500

    Final (Finally)

--- Assignment2.4/sketch.js
+++ Assignment2.4/sketch.js
@@ -1,11 +1,9 @@
 let noise = new Tone.Noise("white");
 let filter = new Tone.Filter(3000, 'highpass');
 
-// Connect noise to filter and filter to destination
 noise.connect(filter);
 filter.toDestination();
 
-// Create envelope for synth
 let envelope = new Tone.Synth({
   oscillator: {
     type: 'square'
@@ -18,7 +16,6 @@ let envelope = new Tone.Synth({
   }
 }).toDestination();
 
-// Create filter slider
 let filterSlider;
 
 function preload() {
@@ -27,8 +24,7 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400);
-
-  // Create the slider
+  
   filterSlider = createSlider(100, 5000, 3000, 1);
   filterSlider.position(150, 200);
   filterSlider.mouseMoved(() => {
@@ -46,13 +42,11 @@ function draw() {
 }
 
 function mousePressed() {
-  // Trigger attack and start noise
   envelope.triggerAttack('C4');
   noise.start();
 }
 
 function mouseReleased() {
-  // Trigger release and stop noise
   envelope.triggerRelease();
   noise.stop();
 }
