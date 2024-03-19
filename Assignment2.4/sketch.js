7bc9e904356e9a940b0a5f4531b9c7eee7b40d00
thomasw33 <144080706+thomasw33@users.noreply.github.com>
2024-03-19 13:39:59 -0500

    assignment2.3

---
+++ Assignment2.4/sketch.js
@@ -0,0 +1,66 @@
+let noise = new Tone.Noise("white");
+let filter = new Tone.Filter(3000, 'highpass');
+
+
+// I put the signal flow here 
+noise.connect(filter);
+filter.toDestination();
+
+
+//maybe change this to a synth object where you define the envelope within it
+let envelope = new Tone.Synth({
+ oscillator: {
+   type: 'square'
+ },
+ envelope: {
+ attack: 0.001,
+ decay: 0.01,  
+ sustain: 0.0,  
+ release: 0.01,
+}
+}).toDestination();
+
+
+
+// let amLFO = new Tone.LFO({
+//   frequency: 2,
+//   amplitude: 0.3,
+// }).start();
+
+
+// amLFO.connect(noise.volume); 
+// filter.connect(envelope);
+
+
+function preload() {
+ flash = loadImage('assets/flash.jpeg');
+}
+
+
+function setup() {
+ createCanvas(400, 400);
+
+
+}
+
+
+function draw() {
+ if (mouseIsPressed) {
+   background(flash);
+ } else {
+   background(240);
+   text('Press Mouse for The Flash', 150, height / 3);
+ }
+}
+
+
+function mousePressed(){
+ envelope.triggerAttack('c4');
+ noise.start();
+}
+
+
+function mouseReleased(){
+ envelope.triggerRelease();
+ noise.stop();
+}
